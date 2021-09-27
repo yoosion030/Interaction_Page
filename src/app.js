dc12b8e9019918f3053fa34d6754ca12b35fc3cb
yoosion <rsy5487@naver.com>
2021-09-27 20:17:22 +0900

    Add JS

---
+++ src/app.js
@@ -0,0 +1,22 @@
+(()=>{
+
+    const sceneInfo = [
+        {   //0
+            heightNum:5, // 브라우저 높이의 5배로 scrollHeight 세팅
+            scrollHeight:0,
+        },
+        {   //1
+            heightNum:5, 
+            scrollHeight:0,
+        },
+        {   //2
+            heightNum:5, 
+            scrollHeight:0,
+        },
+        {   //3
+            heightNum:5, 
+            scrollHeight:0,
+        }
+    ];
+
+})();
