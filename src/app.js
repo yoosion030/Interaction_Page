13841437851f101d304240157f231cf42b99baf1
yoosion <rsy5487@naver.com>
2021-10-04 09:04:51 +0900

    스크롤 애니메이션 구현 4

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@
     let yOffset = 0; // window.pageYoffset 대신 쓸 변수
     let prevScrollHeight = 0; // 현재 스크롤 위치보다 이전에 위치한 스크롤 섹션들의 스크롤 높이값의 합
     let currentScene = 0; // 현재 활성화된 씬
+    let enterNewScene = false; // 새로운 scene이 시작되는 순간 true
 
     const sceneInfo = [{ //0
             type: 'sticky',
@@ -49,13 +50,13 @@
         // 각 스크롤 섹션의 높이 세팅
         for (let i = 0; i < sceneInfo.length; i++) {
             sceneInfo[i].scrollHeight = sceneInfo[i].heightNum * window.innerHeight;
-            sceneInfo[i].objs.container.style.height = `${sceneInfo[i].scrollHeight}px`;
+            sceneInfo[i].objs.container.style.height = `${sceneInfo[i].scrollHeight}px`;// 컨테이너에 높이 값 넣기
         }
 
-        let totalScrollHeight = 0;
+        let totalScrollHeight = 0; // 전체 scrollHeight 값
         yOffset = window.pageYOffset;
         for (let i = 0; i < sceneInfo.length; i++) {
-            totalScrollHeight += sceneInfo[i].scrollHeight;
+            totalScrollHeight += sceneInfo[i].scrollHeight; // 
             if (totalScrollHeight >= yOffset) {
                 currentScene = i;
                 break;
@@ -75,40 +76,42 @@
         const objs = sceneInfo[currentScene].objs;
         const values = sceneInfo[currentScene].values;
         const currentYOffset = yOffset - prevScrollHeight;
+
         switch (currentScene) {
             case 0:
                 let messageA_opacity_in = calcValues(values.messageA_opacity, currentYOffset);
-                objs.messageA.style.opacity = messageA_opacity_in;
-                // console.log(messageA_opacity_in);
-                console.log(objs.messageA.style.opacity);
+                objs.messageA.style.opacity = messageA_opacity_in; // 스크롤 비율의 값을 opacity값에 대입
+                console.log(messageA_opacity_in);
                 break;
             case 1:
-                // calcValues();
                 break;
             case 2:
-                // calcValues();
                 break;
             case 3:
-                // calcValues();
                 break;
         }
-        document.body.setAttribute('id', `show-scene-${currentScene}`);
+        document.body.setAttribute('id', `show-scene-${currentScene}`); // 초기 id값이 없기 때문에 초기값 설정
     }
 
     function scrollLoop() {
+        enterNewScene = false;
         prevScrollHeight = 0;
         for (let i = 0; i < currentScene; i++) {
             prevScrollHeight += sceneInfo[i].scrollHeight;
         }
         if (yOffset > prevScrollHeight + sceneInfo[currentScene].scrollHeight) {
+            enterNewScene = true;
             currentScene++;
             document.body.setAttribute('id', `show-scene-${currentScene}`);
         }
         if (yOffset < prevScrollHeight) {
+            enterNewScene = true;
             if (currentScene === 0) return; // 브라우저 바운스 효과로 인해 마이너스가 되는 것을 방지
             currentScene--;
             document.body.setAttribute('id', `show-scene-${currentScene}`);
         }
+
+        if(enterNewScene) return; // 새로운 페이지에 들어갈 시 함수를 종료함 -> opacity 값이 음수가 나오기 때문에 
         playAnimation();
     }
 
