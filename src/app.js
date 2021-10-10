2466f2b39472d5abf1d29a562b71d0bd83465ecf
yoosion <rsy5487@naver.com>
2021-10-10 23:43:59 +0900

    컨버스 이미지 처리2

--- src/app.js
+++ src/app.js
@@ -62,15 +62,15 @@
 				messageC: document.querySelector('#scroll-section-2 .c'),
 				pinB: document.querySelector('#scroll-section-2 .b .pin'),
 				pinC: document.querySelector('#scroll-section-2 .c .pin'),
-				// canvas: document.querySelector('#video-canvas-1'),
-				// context: document.querySelector('#video-canvas-1').getContext('2d'),
-				// videoImages: []
+				canvas: document.querySelector('#video-canvas-1'),
+				context: document.querySelector('#video-canvas-1').getContext('2d'),
+				videoImages: []
 			},
             values: {
-				// videoImageCount: 960,
-				// imageSequence: [0, 959],
-				// canvas_opacity_in: [0, 1, { start: 0, end: 0.1 }],
-				// canvas_opacity_out: [1, 0, { start: 0.95, end: 1 }],
+				videoImageCount: 960,
+				imageSequence: [0, 959],
+				canvas_opacity_in: [0, 1, { start: 0, end: 0.1 }],
+				canvas_opacity_out: [1, 0, { start: 0.95, end: 1 }],
 				messageA_translateY_in: [20, 0, { start: 0.15, end: 0.2 }],
 				messageB_translateY_in: [30, 0, { start: 0.5, end: 0.55 }],
 				messageC_translateY_in: [30, 0, { start: 0.72, end: 0.77 }],
@@ -109,6 +109,13 @@
 			imgElem.src = `./images/IMG_${6726 + i}.JPG`;
 			sceneInfo[0].objs.videoImages.push(imgElem);
 		}
+		let imgElem2;
+		for (let i = 0; i < sceneInfo[2].values.videoImageCount; i++) {
+			imgElem2 = new Image();
+			imgElem2.src = `./images/IMG_${7027 + i}.JPG`;
+			sceneInfo[2].objs.videoImages.push(imgElem2);
+		}
+
 	}
     function calcValues(values, currentYOffset) {
         let rv;
@@ -192,15 +199,10 @@
 				}
                 break;
             case 2:
-                
-				// if (scrollRatio <= 0.5) {
-				// 	// in
-				// 	objs.canvas.style.opacity = calcValues(values.canvas_opacity_in, currentYOffset);
-				// } else {
-				// 	// out
-				// 	objs.canvas.style.opacity = calcValues(values.canvas_opacity_out, currentYOffset);
-				// }
-				
+				let sequence2 = parseInt(calcValues(values.imageSequence, currentYOffset));
+				objs.context.drawImage(objs.videoImages[sequence2],0,0);
+				objs.canvas.style.opacity = calcValues(values.canvas_opacity, currentYOffset); // 캔버스 투명도 지정
+
 				if (scrollRatio <= 0.25) {
 					// in
 					objs.messageA.style.opacity = calcValues(values.messageA_opacity_in, currentYOffset);
@@ -305,6 +307,7 @@
 		document.body.setAttribute('id',`show-scene-${currentScene}`);// 초기 id값이 없기 때문에 초기값 설정
 		const heightRatio = window.innerHeight / 1080;
 		sceneInfo[0].objs.canvas.style.transform = `translate3d(-50%,-50%, 0) scale(${heightRatio})`;
+		sceneInfo[2].objs.canvas.style.transform = `translate3d(-50%,-50%, 0) scale(${heightRatio})`;
     }
 
     function scrollLoop() {
